7cb55409f2a6226d78f4e78aeb0f9dd8f2297315
Victor Prado <66100225+jvictorprado@users.noreply.github.com>
2020-06-07 01:32:25 -0300

    Update create-point.js
    Limpeza

--- public/scripts/create-point.js
+++ public/scripts/create-point.js
@@ -1,10 +1,3 @@
-/* Função anonima: função que n tem nome..... um tipo de função anonima (algo) => {return banana}   .. arrowfunction*/
-    
-
-/* to usando o fetch, que é uma função que retorna uma promise, vou acessar o site do ibge e qnd voltar, o then vai executar 
-essa função anonima "(resp) => {return resp.json()}" (onde resp é a resposta que o fetch trouxe e o resp.json é basicamente transformar
-ela em um JSON- JavaScript Object Notation). O "resp.json()" tbm é uma promessa; então faremos ele retornar na função. O que me permitirá usar
-o "then" do próprio "resp.json()" (ou seja, ele vai dar uma nova promessa pro "then" de fora) que vai me retornar uma resposta onde vou popular os estados no select */
     function populateStates(){
         const stateSelect = document.querySelector("select[name=uf]");
 
@@ -19,10 +12,6 @@ o "then" do próprio "resp.json()" (ou seja, ele vai dar uma nova promessa pro "
 
     populateStates();
 
-    /* sempre que entrar no change podemos pegar o evento na função */
-    /*o evento traz um monte de informações, uma delas é o target(onde o evento foi executado, que no caso foi no "select[name=state]")
-    dentro do target vc pode pegar o valor dele(value[que por sua vez é aquele "state.id"])*/
-    /*indexOfSelectedState => p armazenar o numero do estado selecionado, que é =>event.target.selectedIndex*/
     function getCities(event){
         const citySelect = document.querySelector("select[name=city]");
         const stateInput = document.querySelector("input[name=state]");
@@ -49,8 +38,6 @@ o "then" do próprio "resp.json()" (ou seja, ele vai dar uma nova promessa pro "
     }
 
     
-/* O addEventListener fica atento p qnd tiver algum evento, como por ex uma mudança "change". O primeiro parâmetro da função é o evento e o
-segundo espera uma função p ele executar */
     document
     .querySelector("select[name=uf]")
     .addEventListener("change",getCities)  
@@ -72,11 +59,7 @@ segundo espera uma função p ele executar */
 
     function handleSelectedItem(event){
         const itemLi = event.target;
-        /* adicionar ou remover uma classe com JS 
-        add > classList.add("selected")
-        remove > classList.remove("selected")
-        toggle > adc ou remove classList.toggle("selected")
-        */
+
         itemLi.classList.toggle("selected")
 
         //vai pegar os numeros que tem no id do li
